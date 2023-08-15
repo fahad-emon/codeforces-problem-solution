1c4f26e9fcfed7a440fe36430294ae3de0f10d64
Fahad Hossain Emon <fahademon.se@gmail.com>
2023-08-15 20:48:25 +0600

    done

---
+++ 1371A_Magical_Sticks.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int t;cin>>t;
+    while (t--)
+    {
+        long long int x;    cin>>x;
+        cout<<((x-1)/2) + 1<<endl;
+    }
+    
+    return 0;
+}
