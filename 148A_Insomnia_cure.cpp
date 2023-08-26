fbfc826bb23917855e0c48a7c60fe7aa07221698
Fahad Hossain Emon <fahademon.se@gmail.com>
2023-08-26 17:12:32 +0600

    done

---
+++ 148A_Insomnia_cure.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int k,l,m,n,d;  cin>>k>>l>>m>>n>>d;
+    int c = 0;
+    for (int i = 1; i <= d; i++)
+    {
+        if(i%k == 0 or i%l == 0 or i%m == 0 or i%n == 0){
+            c++;
+        }
+    }
+    cout<<c<<endl;
+    
+
+    return 0;
+}
