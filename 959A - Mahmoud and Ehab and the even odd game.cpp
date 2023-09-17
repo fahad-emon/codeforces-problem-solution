64bfbc49dcee095045b98faff2315b4c10ce3b56
Fahad Hossain Emon <fahademon.se@gmail.com>
2023-09-18 00:47:58 +0600

    done

---
+++ 959A - Mahmoud and Ehab and the even odd game.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+        int n;          cin>>n;
+
+        if(n < 2){
+            cout<<"Ehab"<<endl;
+        }else{
+            if(n%2 != 0 )
+                cout<<"Ehab"<<endl;
+            else
+                cout<<"Mahmoud"<<endl;
+        }
+        
+    return 0;
+}
